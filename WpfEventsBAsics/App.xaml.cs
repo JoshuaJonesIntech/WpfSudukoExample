a8428c77b612db8c76c1dc207a018949b2884229
jjones <jjonessams@gmail.com>
2020-01-02 15:25:08 +0100

    First commit

---
+++ WpfEventsBAsics/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfEventsBAsics
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+
+			//Show splash screen
+			MessageBox.Show("Application is starting.......");
+
+			// Create the startup window
+			SudukoBoardController wnd = new SudukoBoardController();
+			// Do stuff here, e.g. to the window
+			wnd.Title = "Suduko";
+			// Show the window
+			wnd.Show();
+		}
+	}
+}
---
+++ WpfEventsBAsics/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfEventsBAsics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class SudukoBoardController : Window
+    {
+        public SudukoBoardController()
+        {
+            InitializeComponent();
+            //Subscribing to the MouseUp event within the code file.
+            generateGridButtons();
+        }
+
+        private void generateGridButtons() {
+            for (int i = 0; i < 9; i++) {
+                for (int g = 0; g < 9; g++) {
+                    Button myButton = new Button();
+                    myButton.Name = "GridButton" + i + g;
+                    myButton.Content = "( " + i + ", " + g + " )";
+                    myButton.FontSize = 15;
+                    myButton.Click += gridButtonClicked;
+                    Grid.SetRow(myButton, i);
+                    Grid.SetColumn(myButton, g);
+                    pnlMainGrid.Children.Add(myButton);
+                }
+            }
+        }
+        private void gridButtonClicked(object sender, RoutedEventArgs routedEventArgs) {
+            MessageBox.Show("You clicked me " + sender.ToString());
+
+        }
+    }
+}
